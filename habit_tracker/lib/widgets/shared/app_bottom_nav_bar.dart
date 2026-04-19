a8dfc94f839970946b9076426f03f01530f98974
Sherwin Paul Sabandal <slsabandal@up.edu.ph>
2026-04-19 23:49:34 +0800

    feat: connect statistics and settings page to app navbar buttons

--- habit_tracker/lib/widgets/shared/app_bottom_nav_bar.dart
+++ habit_tracker/lib/widgets/shared/app_bottom_nav_bar.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import '../../utils/app_colors.dart';
+
 import '../../screens/calendar_screen.dart';
+import '../../screens/main_screen.dart';
+import '../../screens/settings_screen.dart';
+import '../../screens/statistics_screen.dart';
+import '../../utils/app_colors.dart';
 
 class AppBottomNavBar extends StatelessWidget {
   final int currentIndex;
@@ -31,44 +35,57 @@ class AppBottomNavBar extends StatelessWidget {
               context,
               icon: Icons.home_rounded,
               index: 0,
-              onTap: () {},
+              onTap: () => _navigateTo(context, 0),
             ),
             _buildNavIcon(
               context,
               icon: Icons.calendar_month_rounded,
               index: 1,
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => const CalendarScreen(),
-                  ),
-                );
-              },
+              onTap: () => _navigateTo(context, 1),
             ),
             _buildNavIcon(
               context,
               icon: Icons.bar_chart_rounded,
               index: 2,
-              onTap: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Stats coming soon')),
-                );
-              },
+              onTap: () => _navigateTo(context, 2),
             ),
             _buildNavIcon(
               context,
               icon: Icons.settings_rounded,
               index: 3,
-              onTap: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Settings coming soon')),
-                );
-              },
+              onTap: () => _navigateTo(context, 3),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _navigateTo(BuildContext context, int targetIndex) {
+    if (targetIndex == currentIndex) return;
+
+    Widget targetScreen;
+
+    switch (targetIndex) {
+      case 0:
+        targetScreen = const MainScreen();
+        break;
+      case 1:
+        targetScreen = const CalendarScreen();
+        break;
+      case 2:
+        targetScreen = const StatisticsScreen();
+        break;
+      case 3:
+        targetScreen = const SettingsScreen();
+        break;
+      default:
+        targetScreen = const MainScreen();
+    }
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => targetScreen),
     );
   }
 
